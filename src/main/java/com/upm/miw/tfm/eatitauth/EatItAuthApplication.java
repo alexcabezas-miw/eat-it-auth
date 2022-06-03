38682c984759e34c18d64cd18ce56245a04b1f14
Alejandro Cabezas Garríguez <alejandro.cabezas@alumnos.upm.es>
2022-06-03 21:19:49 +0200

    Initial commit

---
+++ src/main/java/com/upm/miw/tfm/eatitauth/EatItAuthApplication.java
@@ -0,0 +1,13 @@
+package com.upm.miw.tfm.eatitauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatItAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatItAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upm/miw/tfm/eatitauth/EatItAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upm.miw.tfm.eatitauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatItAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
